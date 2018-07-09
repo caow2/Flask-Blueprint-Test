524c3f49a535b05aecf0f0770538bddaed729149
caow2 <caow2@students.rowna.edu>
2018-07-09 14:46:59 -0400

    Finished Blueprint test & automating Blueprint imports

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 from flask import Flask
-import os
+import sys
 import glob
+import os
 
-path = './files/'
-os.chdir(path)
+#Instead of doing all the imports for the blueprints manually
+#Automate the imports and the register_blueprints
+#A dictionary is needed to access each specific module that was imported
+os.chdir('./files/')
+sys.path.append(os.getcwd())
 
-filenames = []
+app = Flask(__name__)
+
+module_dict = {}
 for file in glob.glob('*.py'):
     name = file[:-3] #excludes .py
-    filenames.append(name)
-    #import name
+    module = __import__(name)
+    module_dict[name] = module
 
-print(filenames)
+for key in module_dict:
+    app.register_blueprint(module_dict[key].page, url_prefix='/%s' % key) #i.e '/user1'
+
+@app.route('/')
+def homepage():
+    output = "<h1>Available pages that are currently available are:</h1>"
+    output += "<p><a href='/user1/'>/user1/</a><br>"
+    output += "<a href='/user1/hello/'>/user1/hello/</a></p>"
+    output += "<p><a href='/user2/'>/user2/</a><br>"
+    output += "<a href='/user2/hello/'>/user2/hello/</a></p>"
+    return output
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- files/user2.py
+++ files/user2.py
@@ -1,7 +1,11 @@
 from flask import Blueprint
 
-page = Blueprint('page', __name__)
+page = Blueprint('user2_page', __name__)
 
 @page.route("/")
 def main():
     return "Welcome user 2"
+
+@page.route("/hello/")
+def hello():
+    return "Hello World! (from user 2)"
